3be94634b08b48be678d1040a99f0651c8105a6b
Trev <arevor@github.noreply.com>
2018-03-04 15:31:55 -0500

    mastery table poplates now

--- populate_champtable.py
+++ populate_champtable.py
@@ -7,10 +7,13 @@ import requests
 import json
 #connect to db file, or create if empty
 conn = sqlite3.connect('masteryi.db')
-#get API_KEY and GET
+#get API_KEY
 with open('api_key', 'r') as infile:
     API_KEY = infile.readline().rstrip()
-r = requests.get('https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=false' +'&api_key=' + API_KEY)
+
+#make request and cache response
+req_url = 'https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=false&api_key='
+r = requests.get(req_url + API_KEY)
 r = r.json()
 #cache response in text file
 with open('cached_response_timestamp', 'w') as outfile:
@@ -19,19 +22,23 @@ with open('cached_response_timestamp', 'w') as outfile:
 #load up cached response, create table if not made
 data = json.load(open('cached_response_timestamp'))
 c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS `CHAMPIONS` (`Name`	TEXT NOT NULL, `Title`	TEXT NOT NULL, `championID`	INTEGER NOT NULL UNIQUE )")
+c.execute("CREATE TABLE IF NOT EXISTS `CHAMPIONS` (`Name`	TEXT NOT NULL, `Title`	TEXT NOT NULL, "
+          "`championID`	INTEGER NOT NULL UNIQUE )")
 conn.commit()
 
 #dump data
 for k, v in data.items():
-    for kk, vv in v.items():
-        print (vv['id'])
-        id = vv['id']
-        name = vv['name']
-        title = vv['title']
+    #values are within the "data" JSON tag
+    for k2, v2 in v.items():
+        id = v2['id']
+        name = v2['name']
+        title = v2['title']
         try:
             c = conn.cursor()
             c.execute("INSERT INTO CHAMPIONS values (?, ?, ?)", (name, title, id))
             conn.commit()
+        #If champion already exists, do nothing
         except sqlite3.IntegrityError:
-            print("dupe")
+            print("")
+conn.close()
+
--- populate_player_mastery.py
+++ populate_player_mastery.py
@@ -1,23 +1,43 @@
 import sqlite3
 import requests
 import json
-#connect to db file, or create if empty
+import argparse
+# connect to db file, or create if empty
 conn = sqlite3.connect('masteryi.db')
-#get API_KEY and GET
+c = conn.cursor()
+
+# get API_KEY  from file, and summoner ID from argv1
 with open('api_key', 'r') as infile:
     API_KEY = infile.readline().rstrip()
+parser = argparse.ArgumentParser()
+parser.add_argument("summonerID")
+args = parser.parse_args()
 
-SUMMONER_ID = 'ayy lmao'
-
-r = requests.get('https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/'+ SUMMONER_ID +'?api_key=' + API_KEY)
+# make the request and cache it in a file
+req_url = 'https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/'
+r = requests.get(req_url + args.summonerID +'?api_key=' + API_KEY)
 r = r.json()
-#cache response in text file
-with open(('cached_mastery_'+ SUMMONER_ID), 'w') as outfile:
+with open(('cached_mastery_'+ args.summonerID), 'w') as outfile:
     json.dump(r, outfile)
 
-#load up cached response, create table if not made
-data = json.load(open('cached_mastery'))
-c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS `masteries` (`Name`	TEXT NOT NULL, `Title`	TEXT NOT NULL, `championID`	INTEGER NOT NULL UNIQUE )")
+# if the table doesnt exist, create it.
+SQL = "CREATE TABLE IF NOT EXISTS `PLAYERMASTERY` (`championLevel` INTEGER NOT NULL, `chestGranted`	TEXT NOT NULL, " \
+      "`championPoints`	INTEGER NOT NULL, `championPointsSinceLastLevel`	INTEGER NOT NULL, `playerID`	" \
+      "INTEGER NOT NULL, `championPointsUntilNextLevel`	INTEGER NOT NULL, `tokensEarned`	INTEGER NOT NULL, " \
+      "`championID`	INTEGER NOT NULL, `lastPlayTime`	INTEGER NOT NULL UNIQUE )"
+c.execute(SQL)
 conn.commit()
 
+# iterate over the response, and dump new values into table
+# lastPlayTime is the unique key, this is my way of 'Upserting' as sqlite3 doesn't support it :(
+data = json.load(open('cached_mastery_' + args.summonerID))
+for row in data:
+    row_data = (row['championLevel'], row['chestGranted'], row['championPoints'], row['championPointsSinceLastLevel']
+                , row['playerId'], row['championPointsUntilNextLevel'], row['tokensEarned'], row['championId'],
+                row['lastPlayTime'])
+    try:
+        c.execute("INSERT INTO PLAYERMASTERY values(?, ?, ?, ?, ?, ?, ?, ?, ?)", (row_data))
+        print ('updated values for ', row['championId'])
+    except sqlite3.IntegrityError:
+        print (row['championId'], ' ignored, values already exist')
+    conn.commit()
